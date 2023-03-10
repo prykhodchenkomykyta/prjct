cf9c4a6d58f4ab7f1180cd2740771b0d28d93f16
Mykyta Prykhodchenko <nikitaprikhodchenko1@gmail.com>
2023-03-10 15:20:47 +0100

    fourth commit

--- backend/csvtoreact.php
+++ backend/csvtoreact.php
@@ -1,35 +1,35 @@
 <?php
 
-// Проверяем, был ли отправлен CSV-файл
+// Check if csv file was sent
 if (isset($_FILES['csv_file'])) {
 
-  // Открываем CSV-файл для чтения
+  // Open CSV for reading
   $csv_file = $_FILES['csv_file']['tmp_name'];
   $handle = fopen($csv_file, 'r');
   
-  // Создаем новый объект SimpleXMLElement
+  // Making new object SimpleXMLElement
   $xml = new SimpleXMLElement('<root/>');
   
-  // Читаем строки из CSV-файла
+  // Read from CSV-file
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
     
-    // Добавляем строку как дочерний элемент XML-элемента
+    // Adding row XML-element
     $row = $xml->addChild('row');
     foreach($data as $index=>$value) {
       
-      // Добавляем каждый столбец CSV-файла как дочерний элемент XML-элемента row
+      // Adding every column CSV-file like child element XML-element row
       $row->addChild('column' . ($index + 1), $value);
     }
   }
   
-  // Закрываем CSV-файл
+  // Closing CSV-file
   fclose($handle);
   
-  // Сохраняем XML-файл на сервере
+  // Saving XML-file on the server
   $xml_file = 'path/to/xml/file.xml';
   $xml->asXML($xml_file);
   
-  // Отправляем данные React в виде JSON
+  // Sending data into React in JSON
   header('Content-Type: application/json');
   echo json_encode(['data' => $xml->asXML()]);
 }
